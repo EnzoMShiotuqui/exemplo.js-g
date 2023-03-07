e4f447af7a61be9dbe03dd918db7eac89dc2f436
EnzoMShiotuqui <RM551508@fiap.com.br>
2023-03-07 11:43:47 -0300

    mudando o js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//esse é um comentário de linha
+
+/**
+ * e aqui é um comentário
+ * de bloco 
+ * 
+ */
+
+console.log("Ola mudo no srcipt.js ")
+console.error("bom dia ")
+console.warn("será?")
+console.info(" vai ser...")
